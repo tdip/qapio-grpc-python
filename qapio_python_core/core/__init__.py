280b059a4e40debd162e06226b4f71327f428380
Ariel Fischer <ariel.fischer@qapio.com>
2021-12-09 17:54:20 +0100

    Increase cpu count

--- qapio_python_core/core/__init__.py
+++ qapio_python_core/core/__init__.py
@@ -1,5 +1,5 @@
 from rx.scheduler import ThreadPoolScheduler
 
-optimal_thread_count = 4  # multiprocessing.cpu_count()
+optimal_thread_count = 6  # multiprocessing.cpu_count()
 
 scheduler = ThreadPoolScheduler(optimal_thread_count)
